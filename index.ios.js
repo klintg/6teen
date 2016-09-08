3d3484897ad4f3650be27cc891595cafed4b1313
klintg <theklint1@gmail.com>
2016-09-08 21:28:59 +0300

    dependecies and route

--- index.ios.js
+++ index.ios.js
@@ -11,43 +11,5 @@ import {
   Text,
   View
 } from 'react-native';
-
-class SixTeen extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('SixTeen', () => SixTeen);
+import app from './App/container/index.js';
+AppRegistry.registerComponent('SixTeen', () => app);
